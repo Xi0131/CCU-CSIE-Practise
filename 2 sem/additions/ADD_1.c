d7b398cf6ed2615d05ba05b84201759be6c24168
Xi0131 <68956855+Xi0131@users.noreply.github.com>
2023-03-14 11:17:49 +0800

    update

---
+++ 2 sem/additions/ADD_1.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+void swap(int *a, int *b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void bubble(int *arr, int size){
+    for(int i = 0; i < size - 1; i++)
+        for(int j = 0; j < size - i - 1; j++)
+            if(arr[j] > arr[j + 1]){
+                swap(arr + j, arr + (j + 1));
+                printf("%d %d\n", arr[j], arr[j+1]);
+            }
+}
+
+void selection(int *arr, int size){
+    for(int i = 0; i < size; i++){
+        int *buffer = &arr[i];
+        for(int j = i; j < size; j++){
+            if(arr[j] < *buffer){
+                buffer = &arr[j];
+            }
+        }
+        if(arr[i] != *buffer){
+            swap(&arr[i], buffer);
+            printf("%d %d\n", arr[i], *buffer);
+        }
+    }
+}
+
+void insertion(int *arr, int size){
+    
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    char type;
+    scanf(" %c", &type);
+    
+    if(type == 'B'){
+        bubble(arr, n);
+    }
+    else if(type == 'S'){
+        selection(arr, n);
+    }
+    else{
+        insertion(arr, n);
+    }
+
+
+    return 0;
+}
