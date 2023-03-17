a7dd5c90674b024e7cb1cacd816ee3fec01d015b
Xi0131 <68956855+Xi0131@users.noreply.github.com>
2023-03-18 01:02:03 +0800

    done hw1

--- 2 sem/hw/HW1_4.c
+++ 2 sem/hw/HW1_4.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 #include <string.h>
+#include <stdlib.h>
+
+#define watch(x) printf("%s is %c\n", (#x), (x))
+#define debug printf("HI\n")
 
 int main(){
 
     char *words[100000];
-    // for(int i = 0; i < 100000; i++){
-    //     words[i] = (char*) malloc(sizeof(char) * 100);
-    //     scanf("%s", words[i]);
-    // }
     int ans = 0;
-    int next = 0;
+    int cnt = 0;
     char buffer;
     do{
-        words[next] = (char*) malloc(sizeof(char) * 100);
-        scanf("%s", words[next]);
-        next++;
-    }while(buffer = getchar() != '\n');
+        words[cnt] = (char*) malloc(sizeof(char) * 50);
+        scanf("%s", words[cnt]);
+        cnt++;
+    }while((buffer = getchar()) != '\n' && buffer != '\r');
+
     char target[100];
     scanf("%s", target);
     int t_len = strlen(target);
-    for(int i = 0; i <= next; i++){
-        if(strlen(*words[i]) == t_len){
+
+    for(int i = 0; i < cnt; i++){
+        if(strlen(words[i]) == t_len){
             int flag = 1;
-            for(int j = 0; j <= t_len; j++){
-                if(*words[i] != target[j]) flag = 0;
+            for(int j = 0; j < t_len; j++){
+                if(words[i][j] != target[j]) flag = 0;
             }
             if(flag) ans++;
         }
     }
+
+    printf("%d", ans);
 
     return 0;
 }
