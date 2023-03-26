cf67e45ef5552f4f279f4df111d6672ae9c99529
Xi0131 <68956855+Xi0131@users.noreply.github.com>
2023-03-27 02:22:14 +0800

    complete ex4

--- 2 sem/exercise/ex4.c
+++ 2 sem/exercise/ex4.c
@@ -1,8 +1,66 @@
 #include <stdio.h>
+#include <string.h>
+
+void create_data(), input_data(), watch_data();
+int criminal_cnt = 0;
+
+struct history{
+    char crime[20];
+    int year;
+    int penalty;
+};
+
+struct criminal{
+    char name[15];
+    struct history crime_his[10];
+    int crime_cnt;
+};
+struct criminal bad_ppl[10];
 
 int main(){
 
-    
+    int o;
+    while(1){
+        printf("Enter a command: ");
+        scanf("%d", &o);
+        if(o == 0) break;
+        void (*cmd[3])() = {create_data, input_data, watch_data};
+        cmd[o-1]();
+    }
 
     return 0;
+}
+
+void create_data(){
+    printf("Enter a criminal's name: ");
+    scanf("%s", bad_ppl[criminal_cnt].name);
+    bad_ppl[criminal_cnt].crime_cnt = 0;
+    printf("Created new criminal data (number %d).\n", criminal_cnt);
+    criminal_cnt++;
+}
+
+void input_data(){
+    int a;
+    printf("Enter a number of the criminal: ");
+    scanf("%d", &a);
+    bad_ppl[a].crime_cnt++;
+    printf("Enter crime: ");
+    scanf(" %s", bad_ppl[a].crime_his[bad_ppl[a].crime_cnt-1].crime);
+    printf("Enter date: ");
+    scanf(" %d", &(bad_ppl[a].crime_his[bad_ppl[a].crime_cnt-1].year));
+    printf("Enter penalty: ");
+    scanf(" %d", &(bad_ppl[a].crime_his[bad_ppl[a].crime_cnt-1].penalty));
+}
+
+void watch_data(){
+    int a;
+    printf("Enter a number of the criminal: ");
+    scanf("%d", &a);
+    printf("Name: %s\n", bad_ppl[a].name);
+    printf("====================\n");
+    for(int i = 0; i < bad_ppl[a].crime_cnt; i++){
+        printf("Year: %d, Crime: %s, Penalty: %d months\n"
+            , bad_ppl[a].crime_his[i].year, bad_ppl[a].crime_his[i].crime, bad_ppl[a].crime_his[i].penalty);
+    }
+    printf("====================\n");
 }
--- 2 sem/hw/HW2_1.c
+++ 2 sem/hw/HW2_1.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct course
-{
-    int id;
-    struct course *next = (struct course*) malloc(sizeof(struct course));
-    struct course *prior;
-    char name[101];
-};
+// struct course
+// {
+//     int id;
+//     int next_id, prior_id;
+//     struct course *next;
+//     struct course *prior;
+//     char name[101];
+// };
 
 
 int main(){
-    printf("hi");
     int num;
+    struct course list;
     scanf("%d", &num);
+    for(int i = 0; i < num-1; i++){
+        list.;
+        // if(i == 0){
+        //     list.prior = NULL;
+        // }
+        // else if(i == num-2){
+        //     list.next = NULL;
+        // }
+        // &list = list.next;
+    }
 
     return 0;
 }
