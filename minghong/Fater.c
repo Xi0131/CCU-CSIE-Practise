06fb02714d3179a412817f518d33cc20365afe4d
Xi0131 <68956855+Xi0131@users.noreply.github.com>
2023-03-31 08:30:14 +0800

    update

--- minghong/Fater.c
+++ minghong/Fater.c
@@ -2,11 +2,13 @@
 
 int main(){
 
+    // input
     int quantity, f_per_hour;
     scanf("%d %d", &quantity, &f_per_hour);
     int time[quantity];
     for(int i = 0; i < quantity; i++) scanf("%d", &time[quantity-1]);
 
+    // swap
     for(int i = 0; i < quantity; i++){
         for(int j = 0; j < quantity - 1 - i; j++){
             if(time[j] > time[j + 1]){
@@ -16,6 +18,9 @@ int main(){
             }
         }
     }
+
+    // 
+    
 
     return 0;
 }
