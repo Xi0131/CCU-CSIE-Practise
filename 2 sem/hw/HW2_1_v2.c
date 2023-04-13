1b167487b3341277ba713a258bd961be10969d5b
Xi0131 <68956855+Xi0131@users.noreply.github.com>
2023-04-13 22:00:17 +0800

    update

--- 2 sem/hw/HW2_1_v2.c
+++ 2 sem/hw/HW2_1_v2.c
@@ -13,28 +13,32 @@ struct course
 };
 
 struct course *create_node(struct course *head){
-    for(; head->course_id != NULL; head = head->next);
-    head = (struct course*) malloc(sizeof(struct course));
+    for(; head->course_name != NULL; head = head->next){
+        head = (struct course*) malloc(sizeof(struct course));
+        break;
+    }
     return head;
 }
 
-struct course *search_node(struct course *head, int tgt_id){
+struct course *search_prior_node(struct course *head, int tgt_id){
     for(; head->course_id != tgt_id; head = head->next);
     return head;
 }
 
 int main(){
 
-    struct course *node = (struct course*) malloc(sizeof(struct course));
-    struct course *head = node;
+    struct course *head = (struct course*) malloc(sizeof(struct course));
     int num;
     scanf("%d", &num);
 
     for(; num > 0; num--){
-        int next, prior;
-        node = create_node(head);
+        int next, t_prior;
+        struct course *node = create_node(head);
+        debug;
+        scanf("%d %d %d %s", &(node->course_id), &next, &t_prior, node->course_name);
         printf("%d", head->course_id);
-        scanf("%d %d %d %s", node->course_id, &next, &prior, node->course_name);
+        node->prior = search_prior_node(head, t_prior);
+        debug;
     }
 
     return 0;   
