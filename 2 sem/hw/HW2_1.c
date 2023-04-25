18676522b3f2edffb7745033a0a39acb771c8037
Xi0131 <68956855+Xi0131@users.noreply.github.com>
2023-04-26 02:34:51 +0800

    update hw2_1

--- 2 sem/hw/HW2_1.c
+++ 2 sem/hw/HW2_1.c
@@ -4,8 +4,8 @@
 
 #define NAME_LENGTH 50
 #define debug printf("Debug line %d\n", __LINE__)
-#define watch_int(x) printf(#x " is %d", x)
-#define watch_string(x) printf(#x " is %s", x)
+#define watch_int(x) printf(#x " is %d\n", x)
+#define watch_string(x) printf(#x " is %s\n", x)
 
 struct course
 {
@@ -13,10 +13,11 @@ struct course
     int course_id;
     struct course *next;
     struct course *prior;
+    int prior_id;
 };
 
-struct course *search(int tgt, struct course *list){
-    for(; list != NULL; list++){
+struct course *search(int num, int tgt, struct course *list){
+    for(int i = 0; (list->next != NULL) && (i < num); list = list->next, i++){
         if(list->course_id == tgt){
             return list;
         }
@@ -24,23 +25,39 @@ struct course *search(int tgt, struct course *list){
     return NULL;
 }
 
-void insert(int id, int next, int prior, char *name, struct course **list){
+void insert(int num, int id, int next, int prior, char *name, struct course **list){
     struct course *buffer = (struct course*) malloc(sizeof(struct course));
     buffer->course_id = id;
     strcpy(buffer->name, name);
-    if(prior == -1) buffer->prior = NULL;
+    buffer->next = NULL;
+    buffer->prior = NULL;
+    buffer->prior_id = prior;
     if(*list == NULL){
         *list = buffer;
     }
     else{
         struct course *temp = *list;
-        for(; temp->next != NULL; temp++);
+        for(; temp->next != NULL; temp = temp->next);
         temp->next = buffer;
-        struct course *prev = search(prior, *list);
-        buffer->prior = prev;
+        struct course *next_node = search(num, next, *list);
+        if(next_node != NULL) buffer->next = next_node;
+        else buffer->next = NULL;
+        // struct course *prev = search(num, prior, *list);
+        // buffer->prior = prev;
     }
 }
 
+void link_prior(int num, struct course **list){
+    struct course *temp2 = *list;
+    for(int i = 0; i < num; temp2 = temp2->next, i++){
+        if(temp2->prior == NULL){
+            struct course *prior_exist = search(num, temp2->prior_id, *list);
+            if(prior_exist != NULL){
+                temp2->prior = prior_exist;
+            }
+        }
+    }
+}
 
 int main(){
 
@@ -50,20 +67,19 @@ int main(){
 
     for(int i = 0; i < num; i++){
         int id, next, prior;
-        char *name;
+        char *name = (char*) malloc(sizeof(char) * NAME_LENGTH);
         scanf("%d %d %d %s", &id, &next, &prior, name);
-        insert(id, next, prior, name, &list);
+        insert(num, id, next, prior, name, &list);
     }
 
+    link_prior(num, &list);
+
+    struct course *buffer = list;
     while(1){
-        struct course *buffer = list;
-        debug;
         printf("Current Course: %s\n", buffer->name);
-        printf("[n] Next course. [p] Prior course [q] Quit:");
+        printf("[n] Next course. [p] Prior course [q] Quit: ");
         char cmd;
-        debug;
-        scanf("%c", &cmd);
-        debug;
+        scanf(" %c", &cmd);
         if(cmd == 'q') break;
         else if(cmd == 'n'){
             if(buffer->next == NULL) printf("There is no next course.\n");
